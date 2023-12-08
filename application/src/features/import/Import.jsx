ca475a70ecb93eb05491474d8a1fadf738c6174d
Cameron Taylor <TheJimmyBlaze@gmail.com>
2023-12-08 13:54:11 +1300

    Moving slice to it's own module

--- application/src/features/import/Import.jsx
+++ application/src/features/import/Import.jsx
@@ -2,24 +2,14 @@
 import { useState } from 'react';
 import { NavDropdown, Modal, Form, InputGroup, Button, Container, Row, Col } from 'react-bootstrap';
 
-import useImport, { slicers } from './useImport';
-
-import onlyNumbers from '../../lib/onlyNumbers';
-import SlicePreview from './SlicePreview';
+import useImport from './useImport';
+import Slice from './Slice';
 
 const Import = ({
 
 }) => {
 
-  const {
-    spriteUrl,
-    setSpriteUrl,
-    slicer,
-    setSlicerByName,
-    dimensions,
-    slicerHasDimensions,
-    clear
-  } = useImport();
+  const importer = useImport();
 
   const [show, setShow] = useState(false);
   const handleClose = () => { clear(); setShow(false); }
@@ -42,122 +32,11 @@ const Import = ({
         <Modal.Header>
           <h4>Import assets from sprite sheet</h4>
         </Modal.Header>
-        
+
         <Modal.Body>
-
-          <Container fluid>
-
-            <Row className="g-2">
-              <Col xs={12}>
-                <Form.Control 
-                  type="file"
-                  onChange={e => {
-                    if (!e.target.files || e.target.files.length === 0) {
-                      return setSpriteUrl(null);
-                    }
-                    setSpriteUrl(e.target.files[0]);
-                  }}
-                />
-              </Col>
-
-              <Col xs={12}>
-
-                <Form.Select
-                  onChange={e => setSlicerByName(e.target.value)}
-                  value={slicer?.name}
-                >
-                  {/* <option value={slicers.prop.name}>Prop slicer</option>
-                  <option value={slicers.animatedProp.name}>Animated prop slicer</option> */}
-                  <option value={slicers.interior.name}>Interior slicer</option>
-                </Form.Select>
-              </Col>
-
-              <Col xs={12} className="p-2"/>
-
-              <Col xs={3}>
-                <InputGroup>
-                  <InputGroup.Text>x</InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    onKeyDown={e => onlyNumbers(e)}
-                    onChange={e => dimensions.setX(e.target.value)}
-                    value={dimensions.x}
-                  />
-                </InputGroup>
-              </Col>
-
-              <Col xs={3}>
-                <InputGroup>
-                  <InputGroup.Text>y</InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    onKeyDown={e => onlyNumbers(e)}
-                    onChange={e => dimensions.setY(e.target.value)}
-                    value={dimensions.y}
-                  />
-                </InputGroup>
-              </Col>
-
-              <Col xs={3}>
-                <InputGroup>
-                  <InputGroup.Text>w</InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    onKeyDown={e => onlyNumbers(e)}
-                    onChange={e => dimensions.setWidth(e.target.value)}
-                    value={dimensions.width}
-                    disabled={!slicerHasDimensions()}
-                  />
-                </InputGroup>
-              </Col>
-
-              <Col xs={3}>
-                <InputGroup>
-                  <InputGroup.Text>h</InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    onKeyDown={e => onlyNumbers(e)}
-                    onChange={e => dimensions.setHeight(e.target.value)}
-                    value={dimensions.height}
-                    disabled={!slicerHasDimensions()}
-                  />
-                </InputGroup>
-              </Col>
-
-              <Col xs={6}>
-                <InputGroup>
-                  <InputGroup.Text>rows</InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    onKeyDown={e => onlyNumbers(e)}
-                    onChange={e => dimensions.setRows(e.target.value)}
-                    value={dimensions.rows}
-                  />
-                </InputGroup>
-              </Col>
-              
-              <Col xs={6}>
-                <InputGroup>
-                  <InputGroup.Text>columns</InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    onKeyDown={e => onlyNumbers(e)}
-                    onChange={e => dimensions.setColumns(e.target.value)}
-                    value={dimensions.columns}
-                  />
-                </InputGroup>
-              </Col>
-
-              <Col xs={12} className="p-2"/>
-
-              <Col xs={12}>
-                  <SlicePreview 
-                    spriteUrl={spriteUrl}
-                    slicer={slicer}
-                  />
-              </Col>
-            </Row>
-          </Container>
+          <Slice 
+            importer={importer}
+          />
         </Modal.Body>
 
         <Modal.Footer className="d-flex justify-content-between">
@@ -168,7 +47,7 @@ const Import = ({
                 <Button variant="danger" onClick={handleClose}>Cancel</Button>
               </Col>
               <Col xs={6}>
-                <Button variant="primary" className="w-100" onClick={() => console.log(slicer)}>Slice</Button>
+                <Button variant="primary" className="w-100" onClick={() => console.log(importer.slicer)}>Slice</Button>
               </Col>
             </Row>
           </Container>
---
+++ application/src/features/import/Slice.jsx
@@ -0,0 +1,140 @@
+
+import { Form, InputGroup, Container, Row, Col } from 'react-bootstrap';
+
+import { slicers } from './useImport';
+
+import onlyNumbers from '../../lib/onlyNumbers';
+import SlicePreview from './SlicePreview';
+
+const Slice = ({
+    importer
+}) => {
+
+    const {
+        spriteUrl,
+        setSpriteUrl,
+        slicer,
+        setSlicerByName,
+        dimensions,
+        slicerHasDimensions,
+        clear
+    } = importer;
+
+    return (
+        <Container fluid>
+
+            <Row className="g-2">
+                <Col xs={12}>
+                <Form.Control 
+                    type="file"
+                    onChange={e => {
+                        if (!e.target.files || e.target.files.length === 0) {
+                            return setSpriteUrl(null);
+                        }
+                        setSpriteUrl(e.target.files[0]);
+                    }}
+                />
+                </Col>
+
+                <Col xs={12}>
+
+                <Form.Select
+                    onChange={e => setSlicerByName(e.target.value)}
+                    value={slicer?.name}
+                >
+                    {/* <option value={slicers.prop.name}>Prop slicer</option>
+                    <option value={slicers.animatedProp.name}>Animated prop slicer</option> */}
+                    <option value={slicers.interior.name}>Interior slicer</option>
+                </Form.Select>
+                </Col>
+
+                <Col xs={12} className="p-2"/>
+
+                <Col xs={3}>
+                <InputGroup>
+                    <InputGroup.Text>x</InputGroup.Text>
+                    <Form.Control
+                    type="number"
+                    onKeyDown={e => onlyNumbers(e)}
+                    onChange={e => dimensions.setX(e.target.value)}
+                    value={dimensions.x}
+                    />
+                </InputGroup>
+                </Col>
+
+                <Col xs={3}>
+                <InputGroup>
+                    <InputGroup.Text>y</InputGroup.Text>
+                    <Form.Control
+                    type="number"
+                    onKeyDown={e => onlyNumbers(e)}
+                    onChange={e => dimensions.setY(e.target.value)}
+                    value={dimensions.y}
+                    />
+                </InputGroup>
+                </Col>
+
+                <Col xs={3}>
+                <InputGroup>
+                    <InputGroup.Text>w</InputGroup.Text>
+                    <Form.Control
+                    type="number"
+                    onKeyDown={e => onlyNumbers(e)}
+                    onChange={e => dimensions.setWidth(e.target.value)}
+                    value={dimensions.width}
+                    disabled={!slicerHasDimensions()}
+                    />
+                </InputGroup>
+                </Col>
+
+                <Col xs={3}>
+                <InputGroup>
+                    <InputGroup.Text>h</InputGroup.Text>
+                    <Form.Control
+                    type="number"
+                    onKeyDown={e => onlyNumbers(e)}
+                    onChange={e => dimensions.setHeight(e.target.value)}
+                    value={dimensions.height}
+                    disabled={!slicerHasDimensions()}
+                    />
+                </InputGroup>
+                </Col>
+
+                <Col xs={6}>
+                <InputGroup>
+                    <InputGroup.Text>rows</InputGroup.Text>
+                    <Form.Control
+                    type="number"
+                    onKeyDown={e => onlyNumbers(e)}
+                    onChange={e => dimensions.setRows(e.target.value)}
+                    value={dimensions.rows}
+                    />
+                </InputGroup>
+                </Col>
+                
+                <Col xs={6}>
+                <InputGroup>
+                    <InputGroup.Text>columns</InputGroup.Text>
+                    <Form.Control
+                    type="number"
+                    onKeyDown={e => onlyNumbers(e)}
+                    onChange={e => dimensions.setColumns(e.target.value)}
+                    value={dimensions.columns}
+                    />
+                </InputGroup>
+                </Col>
+
+                <Col xs={12} className="p-2"/>
+
+                <Col xs={12}>
+                    <SlicePreview 
+                    spriteUrl={spriteUrl}
+                    slicer={slicer}
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Slice;
